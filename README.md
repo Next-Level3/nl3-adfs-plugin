163c11593487cf82e7743f264393909f35870e3c
David Hazar <david@nextlevel3.com>
2023-04-19 00:03:33 +0000

    Add project files.

---
+++ README.md
@@ -0,0 +1,30 @@
+
+# Build Plug-ins with AD FS 2019 Risk Assessment Model
+
+You can now build your own plug-ins to block or assign a risk score to authentication requests during various stages – request received, pre-authentication and post-authentication. This can be accomplished using the new Risk Assessment Model introduced with AD FS 2019. 
+
+## What is the Risk Assessment Model?
+
+The Risk Assessment Model is a set of interfaces and classes which enable developers to read authentication request headers and implement their own risk assessment logic. The implemented code (plug-in) then runs in line with AD FS authentication process. For eg, using the interfaces and classes included with the model, you can implement code to either block or allow authentication request based on the client IP address included in the request header. AD FS will execute the code for each authentication request and take appropriate action as per the implemented logic.
+
+For more details please visit [AD FS Risk Assessment Model documentation](https://docs.microsoft.com/en-us/windows-server/identity/ad-fs/development/ad-fs-risk-assessment-model)
+
+## About this sample
+
+This sample plug-in is meant to better understand how to build a risk assessment plug-in and run it in line with AD FS process. The code in this sample uses the new interfaces and classes introduced with the risk assessment model to block the requests coming from certain extranet IPs identified as risky. 
+
+To learn how to build this sample plug-in please visit [Building a sample plug-in documentation](https://docs.microsoft.com/en-us/windows-server/identity/ad-fs/development/ad-fs-risk-assessment-model#building-a-sample-plug-in)
+
+## Contributing
+
+This project welcomes contributions and suggestions.  Most contributions require you to agree to a
+Contributor License Agreement (CLA) declaring that you have the right to, and actually do, grant us
+the rights to use your contribution. For details, visit https://cla.microsoft.com.
+
+When you submit a pull request, a CLA-bot will automatically determine whether you need to provide
+a CLA and decorate the PR appropriately (e.g., label, comment). Simply follow the instructions
+provided by the bot. You will only need to do this once across all repos using our CLA.
+
+This project has adopted the [Microsoft Open Source Code of Conduct](https://opensource.microsoft.com/codeofconduct/).
+For more information see the [Code of Conduct FAQ](https://opensource.microsoft.com/codeofconduct/faq/) or
+contact [opencode@microsoft.com](mailto:opencode@microsoft.com) with any additional questions or comments.
---
+++ UserRiskAnalyzer.cs
@@ -0,0 +1,329 @@
+﻿using Microsoft.IdentityServer.Public.ThreatDetectionFramework;
+using Microsoft.VisualBasic.FileIO;
+using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.IdentityServer.Public;
+using System.Security.Claims;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+using System.IdentityModel.Tokens.Jwt;
+using System.IdentityModel;
+using Microsoft.IdentityModel.Tokens;
+using System.Web;
+
+namespace ThreatDetectionModule
+{
+    /// <summary>
+    /// IPConverter is a class for fixing the serialization of NetworkLocation objects
+    /// </summary>
+    //public class IPConverter : JsonConverter<IPAddress>
+    public class IPConverter : JsonConverter<IPAddress>
+    {
+        public override void WriteJson(JsonWriter writer, IPAddress value, JsonSerializer serializer)
+        {
+            writer.WriteValue(value.ToString());
+        }
+
+        public override IPAddress ReadJson(JsonReader reader, Type objectType, IPAddress existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            var s = (string)reader.Value;
+            return IPAddress.Parse(s);
+        }
+    }
+    /// <summary>
+    /// UserRiskAnalyzer is the main class implementing the ThreatDetectionModule abstract class and IPreAuthenticationThreatDetectionModule interface.
+    /// During registration of the module with ADFS, pass a config file with list of Client IDs and Signing Keys seperated with ","
+    /// This module will check with Next Level3 to determine if the current user account status is locked or unlocked.
+    /// If the acocunt is locked, the method returns Throttelstatus as 2 (Block), else it returns 1 (Allow).
+    /// </summary>
+    //public class UserRiskAnalyzer : Microsoft.IdentityServer.Public.ThreatDetectionFramework.ThreatDetectionModule, IRequestReceivedThreatDetectionModule
+    public class UserRiskAnalyzer : Microsoft.IdentityServer.Public.ThreatDetectionFramework.ThreatDetectionModule, IPreAuthenticationThreatDetectionModule
+    {
+        private JObject APCConfig;
+        public override string VendorName => "Microsoft";
+        public override string ModuleIdentifier => "UserRiskAnalyzer";
+
+        /// <summary>
+        /// ADFS calls this method while loading the module and it passes the contents of Config file through configData
+        /// This method caches the IPs from it so that it can used when authentication requests are evaluated
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="configData"></param>
+        public override void OnAuthenticationPipelineLoad(ThreatDetectionLogger logger, ThreatDetectionModuleConfiguration configData)
+        {
+            try
+            {
+                ReadConfigFile(logger, configData);
+            }
+            catch (Exception ex)
+            {
+                logger.WriteAdminLogErrorMessage(ex.ToString());
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Stored config data stream
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="configData"></param>
+        private void ReadConfigFile(ThreatDetectionLogger logger, ThreatDetectionModuleConfiguration configData)
+        {
+            APCConfig = new JObject();
+            using (StreamReader sr = new StreamReader(configData.ReadData()))
+            {
+                string line;
+                string headerLine;
+                headerLine = sr.ReadLine();
+                string[] headers;
+                headers = headerLine.Split(',');
+                if (headerLine != null)
+                {
+                    JArray jObjects = new JArray();
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        string[] parts = line.Split(',');
+                        if (parts[0] != null)
+                        {
+                            JObject jObject = new JObject();
+                            int i = 0;
+                            foreach (string part in parts)
+                            {
+                                jObject[headers[i].Trim()] = part.Trim();
+                                i++;
+                            }
+                            jObjects.Add(jObject);
+                        }
+                    }
+                    APCConfig["applications"] = jObjects;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Parses the config file and store it in JObject
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="apcConfig"></param>
+        /// <param name="clientId"></param>
+        private string GetSigningKey(ThreatDetectionLogger logger, String lookupKey, out String appName, out String apiUrl, out String apiVersion, out string ipInfoToken)
+        {
+            JToken applicationToken = APCConfig.SelectToken("$.applications[?(@.LookupKey == '" + lookupKey + "')]");
+            if (applicationToken.HasValues)
+            {
+                JObject application = applicationToken.ToObject<JObject>();
+                JToken signingKey;
+                JToken tAppName;
+                JToken tAPIUrl;
+                JToken tAPIVersion;
+                JToken tIPInfoToken;
+                if (application.TryGetValue("SigningKey", out signingKey) && application.TryGetValue("AppName", out tAppName) && application.TryGetValue("APIUrl", out tAPIUrl) && application.TryGetValue("APIVersion", out tAPIVersion) && application.TryGetValue("IPInfoToken", out tIPInfoToken))
+                {
+                    string sk = signingKey.Value<string>();
+                    appName = tAppName.Value<string>();
+                    apiUrl = tAPIUrl.Value<string>();
+                    apiVersion = tAPIVersion.Value<string>();
+                    ipInfoToken = tIPInfoToken.Value<string>();
+                    return sk;
+                }
+                else
+                {
+                    logger?.WriteAdminLogErrorMessage($"TryGetValue failed!");
+                }
+            }
+            appName = "";
+            apiUrl = "";
+            apiVersion = "";
+            ipInfoToken = "";
+            return null;
+        }
+
+        public override void OnAuthenticationPipelineUnload(ThreatDetectionLogger logger)
+        {
+        }
+
+        /// <summary>
+        /// ADFS calls this method when there is any change in the configuration. This typically happens when Import-AdfsThreatDetectionModuleConfiguration cmdlet is executed
+        /// ADFS passes the contents of Config file through configData
+        /// This method caches the IPs from it so that it can used when authentication requests are evaluated
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="configData"></param>
+        public override void OnConfigurationUpdate(ThreatDetectionLogger logger, ThreatDetectionModuleConfiguration configData)
+        {
+            ReadConfigFile(logger,configData);
+        }
+
+        /// <summary>
+        /// Implements the interface method. 
+        /// This method checks the lock status in NL3 for the user that is authenticating.
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="requestContext"></param>
+        /// <param name="securityContext"></param>
+        /// <param name="protocolContext"></param>
+        /// <param name="additionalClaims"></param>
+        /// <returns></returns>
+        public Task<ThrottleStatus> EvaluatePreAuthentication(ThreatDetectionLogger logger, RequestContext requestContext, SecurityContext securityContext, ProtocolContext protocolContext, IList<Claim> additionalClaims)
+        {
+            try
+            {
+                var referer = requestContext.Headers.Get("Referer");
+                string username = securityContext.UserIdentifier;
+                logger?.WriteAdminLogErrorMessage($"Referer = {referer}");
+                Uri refererUri = new Uri(referer);
+                string issuerOrClientId = null;
+                string [] mSamlRequestCookies;
+                string mSamlRequest = "";
+                string appName;
+                string apiVersion;
+                string apiUrl;
+                string ipInfoToken;
+                if (requestContext.Headers.HasKeys())
+                {
+                    string cookieHeader = requestContext.Headers.Get("Cookie");
+                    if (cookieHeader.Length > 0)
+                    {
+                        Match M = Regex.Match(cookieHeader, string.Format("{0}=(?<value>.*?)$", "MSISSamlRequest"));
+                        if (M.Success)
+                        {
+                            mSamlRequestCookies = M.ToString().Split(';');
+                            foreach (string cookie in mSamlRequestCookies)
+                            {
+                                mSamlRequest += cookie.Split(new char[] { '=' }, 2)[1];
+                            }
+                            mSamlRequest = HttpUtility.UrlDecode(Encoding.UTF8.GetString(Convert.FromBase64String(mSamlRequest)));
+                            M = Regex.Match(mSamlRequest, string.Format("(?<={0})(?<value>[A-Za-z0-9+\\/]{{4}})*(?:[A-Za-z0-9+\\/]{{4}}|[A-Za-z0-9+\\/]{{3}}=|[A-Za-z0-9+\\/]{{2}}={{2}}?)(?=\\\\)", "SAMLRequest="));
+                            if (M.Success)
+                            {
+                                mSamlRequest = Encoding.UTF8.GetString(Convert.FromBase64String(M.ToString()));
+                                XmlDocument xmlDoc = new XmlDocument();
+                                xmlDoc.LoadXml(mSamlRequest);
+                                XmlNode issuerNode = xmlDoc.SelectSingleNode("/");
+                                issuerOrClientId = issuerNode.InnerText;
+                                if ((username == null || username.Length == 0) && referer.Contains("microsoftonline"))
+                                {
+                                    username = HttpUtility.ParseQueryString(refererUri.Query).Get("login_hint");
+                                }
+                            }
+                        }
+                    }
+                }
+                if (issuerOrClientId == null)
+                {
+                    issuerOrClientId = protocolContext.ClientId;
+                }
+                if (issuerOrClientId == null || issuerOrClientId.Length == 0 && !referer.Contains("microsoftonline"))
+                {
+                    issuerOrClientId = HttpUtility.ParseQueryString(refererUri.Query).Get("client_id");
+                }
+                if ((issuerOrClientId == null || issuerOrClientId.Length == 0) && referer.Contains("microsoftonline"))
+                {
+                    issuerOrClientId = "urn:federation:MicrosoftOnline";
+                    if ((username == null || username.Length == 0))
+                    {
+                        username = HttpUtility.ParseQueryString(refererUri.Query).Get("login_hint");
+                    }
+                }
+                logger?.WriteAdminLogErrorMessage($"Issuer or Client ID = {issuerOrClientId}");
+                string base64SigningKey = GetSigningKey(logger, issuerOrClientId, out appName, out apiUrl, out apiVersion, out ipInfoToken);
+                if (base64SigningKey != null && base64SigningKey.Length > 5)
+                {
+                    byte[] key = Convert.FromBase64String(base64SigningKey);
+                    SymmetricSecurityKey securityKey = new SymmetricSecurityKey(key);
+
+                    JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+                    JwtSecurityToken token = handler.CreateJwtSecurityToken(appName, apiUrl, new ClaimsIdentity(new[] {
+                          new Claim("sub", username)}), DateTime.UtcNow.AddMinutes(-1), DateTime.UtcNow.AddMinutes(5), DateTime.UtcNow.AddMinutes(-1), new SigningCredentials(securityKey,
+                        SecurityAlgorithms.HmacSha256Signature));
+
+                    HttpWebRequest requestIPDetails = (HttpWebRequest)WebRequest.Create("https://ipinfo.io/" + requestContext.ClientIpAddresses[0].ToString() + "/json?token=" + ipInfoToken);
+                    JObject jsonIPDetails = new JObject();
+                    string location = "";
+                    using (HttpWebResponse responseIPDetails = (HttpWebResponse)requestIPDetails.GetResponse())
+                    using (Stream stream = responseIPDetails.GetResponseStream())
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        jsonIPDetails = JObject.Parse(reader.ReadToEnd());
+                        if (jsonIPDetails.HasValues)
+                        {
+                            location = jsonIPDetails["city"] + ", " + jsonIPDetails["region"];
+                        }
+                    }
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://" + apiUrl + "/nl3/api/" + apiVersion + "/accountProtectionCheck");
+                    var jsonSettings = new JsonSerializerSettings();
+                    jsonSettings.Converters.Add(new IPConverter());
+                    JObject jPostData = new JObject
+                    {
+                        ["userIP"] = requestContext.ClientIpAddresses[0].ToString(),
+                        ["userDevice"] = requestContext.UserAgentString,
+                        ["userLocation"] = location,
+                        ["integrationType"] = "adfs"
+                    };
+                    jPostData.Add(new JProperty("integrationData", JObject.Parse(JsonConvert.SerializeObject(requestContext, Newtonsoft.Json.Formatting.Indented, jsonSettings))));
+                    jPostData["integrationData"]["locationInfo"] = jsonIPDetails;
+                    logger?.WriteAdminLogErrorMessage($"jPostData = {jPostData.ToString()}");
+                    byte[] byteArray = Encoding.UTF8.GetBytes(jPostData.ToString());
+                    request.Method = "POST";
+                    request.ContentType = "application/json";
+                    request.ContentLength = byteArray.Length;
+                    request.Headers.Add("x-nl3-authorization-token", handler.WriteToken(token));
+                    request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+                    Stream requestStream = request.GetRequestStream();
+                    requestStream.Write(byteArray, 0, byteArray.Length);
+                    requestStream.Close();
+
+                    using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                    using (Stream stream = response.GetResponseStream())
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        JObject jsonResponse = JObject.Parse(reader.ReadToEnd());
+                        JToken locked;
+                        if (jsonResponse.TryGetValue("locked", out locked))
+                        {
+                            if (locked.Value<bool>())
+                            {
+                                logger?.WriteAdminLogErrorMessage($"Locked Status = True");
+                                return Task.FromResult<ThrottleStatus>(ThrottleStatus.Block);
+                            }
+                            else
+                            {
+                                logger?.WriteAdminLogErrorMessage($"Locked Status = False");
+                                return Task.FromResult<ThrottleStatus>(ThrottleStatus.Allow);
+                            }
+                        }
+                        else
+                        {
+                            if (username == null || username.Length == 0)
+                            {
+                                logger?.WriteAdminLogErrorMessage($"Username Missing!");
+                                return Task.FromResult<ThrottleStatus>(ThrottleStatus.Block);
+                            }
+                            else
+                            {
+                                logger?.WriteAdminLogErrorMessage($"Locked Status = False");
+                                return Task.FromResult<ThrottleStatus>(ThrottleStatus.Allow);
+                            }
+                        }
+                    }
+
+                } else
+                {
+                    logger?.WriteAdminLogErrorMessage($"No signing key identified for Client ID = " + issuerOrClientId + ", please check configuration file!");
+                    return Task.FromResult<ThrottleStatus>(ThrottleStatus.Allow);
+                }
+            }
+            catch (Exception ex)
+            {
+                logger?.WriteAdminLogErrorMessage($"Exception = " + ex.ToString());
+                return Task.FromResult<ThrottleStatus>(ThrottleStatus.Block);
+            }
+        }
+    }
+}
